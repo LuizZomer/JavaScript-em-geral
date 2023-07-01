9f75079887ac8ba8495391f87d17ca878fe79b38
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-01 14:28:32 -0300

    Create variavel.js

---
+++ Basico/variavel.js
@@ -0,0 +1,13 @@
+let x;
+let y = '5';
+const pi = 3.14; //Constante
+console.log(x);
+x = 20;
+let total = x * 3.14;
+console.log(x);
+console.log(typeof total);
+
+let z = Number(y)
+
+console.log(typeof z)
+ 
