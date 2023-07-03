c129db903e1b8462c04a98412a4d78d9bb14227f
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-03 11:09:16 -0300

    fim do basico

--- Basico/exercicio/form/script.js
+++ Basico/exercicio/form/script.js
@@ -6,12 +6,16 @@ function meuEscopo() {
 
     function recebeEventoForm (evento){
         evento.preventDefault();
-        const nome = form.querySelector('#nome')
-        const sobrenome = form.querySelector('#sobrenome')
-        const peso = form.querySelector('#peso')
-        const altura = form.querySelector('#altura')
-
+        const nome = form.querySelector('#nome').value
+        const sobrenome = form.querySelector('#sobrenome').value
+        const peso = form.querySelector('#peso').value
+        const altura = form.querySelector('#altura').value
         
+        pessoas.push({
+            nome,sobrenome,peso,altura
+        })
+        console.log(pessoas)
+        resultado.innerHTML += `<p>${nome} ${sobrenome},${peso},${altura} </p>`
     }
     form.addEventListener('submit', recebeEventoForm);
 }
