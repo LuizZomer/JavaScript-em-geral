894107972db3883e564f0783e24ac5d6580ab76b
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-03 10:26:21 -0300

    continuação

--- Basico/exercicio/form/script.js
+++ Basico/exercicio/form/script.js
@@ -1,5 +1,8 @@
 function meuEscopo() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('#resultado')
+
+    const pessoas = []
 
     function recebeEventoForm (evento){
         evento.preventDefault();
@@ -7,8 +10,8 @@ function meuEscopo() {
         const sobrenome = form.querySelector('#sobrenome')
         const peso = form.querySelector('#peso')
         const altura = form.querySelector('#altura')
+
         
-        console.log(nome.value,sobrenome,peso,altura)
     }
     form.addEventListener('submit', recebeEventoForm);
 }
