66f8889d6fa5218393becc7261ac84f94e384152
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-05 10:25:20 -0300

    Create for.js

---
+++ Basico/for.js
@@ -0,0 +1,14 @@
+// for (let i=0; i<=10;i++) {
+//     const par = i% 2 ===0 ? 'Par': 'Impár';
+//     console.log(i,par)
+// }
+
+// for (let i=10; i>= 0;i--) {
+//     console.log(`Olá ${i}`)
+// }
+
+const frutas = ['Maçã','Uva','Pera']
+
+for (let i = 0; i < frutas.length;i++){
+    console.log(frutas[i])
+}
