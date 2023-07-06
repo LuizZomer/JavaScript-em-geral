c6406c9c75a4b0a3994f13c44ce502f852cb3140
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-06 10:23:24 -0300

    exercicio for

---
+++ for/forIn.js
@@ -0,0 +1,14 @@
+const frutas = ['Maçã','Pera','Laranja','Uva'];
+const pessoa = {
+    nome:'Luiz',
+    sobrenome:'Zomer',
+    idade:19
+};
+
+
+// for (let i in frutas){
+//     console.log(frutas[i]);
+// }
+for (let i in pessoa){
+    console.log(pessoa[i])
+}
---
+++ for/forOf.js
@@ -0,0 +1,6 @@
+const nome = ['Luiz','Daniel','Arthur','Lourenço'];
+
+
+for (let valor of nome){
+    console.log(valor)
+}
