a6fd2f0a3fc2900c5f75abb130e952e541b08f1d
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-07 10:56:13 -0300

    timer

--- Basico/exercicio/timer/assets/js/main.js
+++ Basico/exercicio/timer/assets/js/main.js
@@ -1,88 +1,41 @@
-// function zero(data){
-//     return data > 10 ? data : `0${data}`
-// }
-
-
-// const data = new Date()
-// let diaSemana = data.getDay()
-// let dia = zero(data.getDate())
-// let mes = data.getMonth()
-// let ano = data.getFullYear()
-// let hora = zero(data.getHours())
-// let minuto = zero(data.getMinutes())
-
-
-
-// switch (diaSemana){
-//     case 0:
-//         diaSemana = 'Domingo'
-//         break
-//     case 1:
-//         diaSemana = 'Segunda-Feira'
-//         break
-//     case 2:
-//         diaSemana = 'Terça-Feira'
-//         break
-//     case 3:
-//         diaSemana = 'Quarta-Feira'
-//         break
-//     case 4:
-//         diaSemana = 'Quinta-Feira'
-//         break
-//     case 5:
-//         diaSemana = 'Sexta-Feira'
-//         break
-//     case 6:
-//         diaSemana = 'Sábado'
-// }
-
-// switch (mes){
-//     case 0:
-//         mes = 'Janeiro'
-//         break
-//     case 1:
-//         mes = 'fevereiro'
-//         break
-//     case 2:
-//         mes = 'Março'
-//         break
-//     case 3:
-//         mes = 'Abril'
-//         break
-//     case 4:
-//         mes = 'Maio'
-//         break
-//     case 5:
-//         mes = 'Junho'
-//         break
-//     case 6:
-//         mes = 'Julho'
-//         break
-//     case 7:
-//         mes = 'Agosto'
-//         break
-//     case 8:
-//         mes = 'Setembro'
-//         break
-//     case 9:
-//         mes = 'Outubro'
-//         break
-//     case 10:
-//         mes = 'Novembro'
-//         break
-//     case 11:
-//         mes = 'Dezembro'
-//         break
-// }
-
-// let texto = document.querySelector('#texto')
-// texto.innerHTML = `${diaSemana}, ${dia} de ${mes} de ${ano} <br>${hora}:${minuto} `
-
-const h1 = document.querySelector('#texto');
-const data = new Date();
-const opcoes = {
-    dateStyle: 'full',
-    timeStyle : 'short'
+function getTimeFromSeconds(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-br', {
+        hour12:false,
+        timeZone: 'GMT'
+    })
 }
 
-h1.innerHTML = data.toLocaleString('pt-br', opcoes)
+
+const relogio = document.querySelector('#relogio');
+const iniciar = document.querySelector('#iniciar');
+const pausar = document.querySelector('#pausar');
+const zerar = document.querySelector('#zerar');
+let segundos = 0;
+let timer;
+
+function startClock(){
+    timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = getTimeFromSeconds(segundos);
+    },1000)
+}
+
+
+iniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado')
+    clearInterval(timer);
+    startClock()
+})
+
+pausar.addEventListener('click', function(evento){
+    clearInterval(timer);
+    relogio.classList.add('pausado')
+})
+
+zerar.addEventListener('click', function(evento){
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    relogio.classList.remove('pausado')
+    segundos = 0;
+});
