5f659ca5e426059054a2f25cb2f7222684d2aed6
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-07 15:53:04 -0300

    declarando funçao

---
+++ funcao/declarar.js
@@ -0,0 +1,19 @@
+function ola(){
+    console.log('Olá');
+}
+
+const souUmDado = function(){
+    console.log('Sou um dado');
+}
+
+const arrow = () => {
+    console.log('Sou uma arrowFunction')
+}
+
+const objeto = {
+    falar(){
+        console.log('Estou falando...')
+    }
+}
+
+objeto.falar()
