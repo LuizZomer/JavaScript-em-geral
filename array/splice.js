ae18e0a8a4b049aa7066985cdbf958b4c2096c61
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-07-10 20:17:39 -0300

    Create splice.js

---
+++ array/splice.js
@@ -0,0 +1,7 @@
+const nomes = ['Maria','João','Eduardo','Gabriel','Julia'];
+
+// const [a] = nomes.splice(-1,1) // splice(indice, delete, elementos)
+
+nomes.splice(nomes.length,0,'Luiz')
+// console.log(a)
+console.log(nomes)
